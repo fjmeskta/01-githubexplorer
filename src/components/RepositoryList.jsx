605fc0cda55afeee0c5274f18341704fc3cfa7ed
Fábio José Mesquita <fjmeskta@gmail.com>
2021-05-06 21:13:14 -0300

    componente RepositoryList

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,32 @@
+const repositoryName = 'unform 2'
+export function RepositoryList() {
+  return (
+    <section id="repositoryList">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms In React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms In React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms In React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+
+}
